85e6c20c8ed3188ee4b1f9e91621d1347966f984
Natalia Ramirez <natalia16@berkeley.edu>
2023-07-19 16:28:01 -0700

    add functions for computing frame key points/delaunay sets

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from delaunay import run_delaunay, get_delaunay
+from frames import *
 import json
 import numpy as np
-
-IMAGE_NAMES = ["A", "B"]
-
-IMAGE_DIR = "./cropped"
-DATA_DIR = "./data"
+from frames import get_frame_points, get_delaunay
 
 points_dict = {}
+
 for name in IMAGE_NAMES:
     image_path = f"./{IMAGE_DIR}/{name}.jpg"
     with open(f"./{DATA_DIR}/{name}.json") as json_file:
         points_dict[name] = json.load(json_file)
+
+get_frame_points(points_dict['A'], points_dict['B'])
